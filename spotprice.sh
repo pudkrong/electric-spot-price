fa637c8b2016ac28257361e9e8752ab377275e38
Pisut Sritrakulchai <pudkrong@hotmail.com>
2024-10-05 11:18:04 +0200

    feat: using 3 decimal percision

--- spotprice.sh
+++ spotprice.sh
@@ -16,9 +16,9 @@ fi
 unit=$(echo $curl_output | jq -r ".unit")
 current_date=$(echo $curl_output | jq -r ".current.startDateTime")
 current_time=$(echo $curl_output | jq -r ".current.interval")
-current_price=$(echo $curl_output | jq -r ".current.price")
-today_min=$(echo $curl_output | jq -r ".todayMin.price")
-today_max=$(echo $curl_output | jq -r ".todayMax.price")
+current_price=$(echo $curl_output | jq -r ".current.price | tonumber | (.*1000 | round) / 1000")
+today_min=$(echo $curl_output | jq -r ".todayMin.price | tonumber | (.*1000 | round) / 1000")
+today_max=$(echo $curl_output | jq -r ".todayMax.price | tonumber | (.*1000 | round) / 1000")
 
 normalized_date=$(echo "$current_date" | sed 's/T.*$//')
 
@@ -28,5 +28,6 @@ Current: ${current_price} ${unit}
 Today min: ${today_min} ${unit}
 Today max: ${today_max} ${unit}"
 
+echo $message
 # Send notification to LINE notify
 curl -XPOST https://notify-api.line.me/api/notify -H "Authorization: Bearer $ACCESS_TOKEN" -F "message=$message"
